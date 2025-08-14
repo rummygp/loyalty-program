3f3872b0ceeb0a27aba133525132983460938372
egorummy <adam.mokrzycki22@gmail.com>
2025-08-14 21:53:59 +0200

    initial commit

---
+++ src/test/java/com/rummygp/loyalty_program/LoyaltyProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rummygp.loyalty_program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
